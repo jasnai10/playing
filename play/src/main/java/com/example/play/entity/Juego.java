fe18051ddb855e64d496d468288db3bc7a799aa6
jasnai10 <a20223806@pucp.edu.pe>
2025-11-27 12:24:03 -0500

    listo para listar

--- play/src/main/java/com/example/play/entity/Juego.java
+++ play/src/main/java/com/example/play/entity/Juego.java
@@ -12,4 +12,32 @@ public class Juego {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idjuego", nullable = false)
     private Integer id;
+
+    @Column(name = "nombre", length =  50)
+    private String nombre;
+
+    @Column(name = "descripcion", length = 448)
+    private String descripcion;
+
+    @Column(name = "precio")
+    private Double precio;
+
+    @Column(name = "image", length = 400)
+    private String image;
+
+    @ManyToOne
+    @JoinColumn(name = "idgenero")
+    private Genero genero;
+
+    @ManyToOne
+    @JoinColumn(name = "idplataforma")
+    private Plataforma plataforma;
+
+    @ManyToOne
+    @JoinColumn(name = "iddistribuidora")
+    private Distribuidora distribuidora;
+
+    @ManyToOne
+    @JoinColumn(name = "ideditora")
+    private Editora editora;
 }
